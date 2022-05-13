2bebcd645609cd31ab62c1073e7d0d3052cbc642
John Roughley <jroughley@softwareinstitute.com>
2022-05-13 16:05:58 +0100

    tidied main and shop source files

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,10 @@
 #include<thread>
 #include"../inc/shop.hpp"
 
-
 int main()
 {
     Barbershop BS; 
-
-    //std::thread barberThread(&Barbershop::BarberProcess, &BS);
-/*     std::thread barberThread(&Barbershop::BarberProcess, &BS, std::ref(BS.GetBarbers().back()));
-    std::thread customerThread(&Barbershop::CustomerProcess, &BS);
-
-    barberThread.join();
-    customerThread.join(); */
-
-/*     std::thread bThread = BS.CreateBarberThread();
-    std::thread cThread = BS.CreateCustomerThread();
-
-    bThread.join();
-    cThread.join(); */
-
-/*     std::thread bThread = BS.CreateBarberThread();
-    std::thread cThread = BS.CreateCustomerThread();
-    bThread.join();
-    cThread.join(); */
-
-    BS.CreateBarberThread();
-    BS.CreateCustomerThread();
-
-    int input = 0;
-    std::cin >> input;
+    BS.Run();
 
     return 0;
 }
